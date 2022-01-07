48df581d1d81551fcf385e478e3db5154c9c9325
Vipulbhj <vipulb@geekyants.com>
2022-01-08 00:47:30 +0530

    Fixed useInput handler reading mouse codes

--- src/hooks/use-edit.ts
+++ src/hooks/use-edit.ts
@@ -1,14 +1,14 @@
-import { useInput } from 'ink';
+import { useInput } from "ink";
 import { useEffect, useState } from "react";
-import { AppState, SetStateFn } from '../utils';
-import { BufferCommands } from './use-buffer';
+import { AppState, SetStateFn } from "../utils";
+import { BufferCommands } from "./use-buffer";
 
 const hexRegex = /[0-9a-f]/;
 const isHexChar = (char: string) => hexRegex.test(char);
 
 enum CommandChar {
-  InsertByteAtCursor    = 'i',
-  InsertByteAfterCursor = 'I',
+  InsertByteAtCursor = "i",
+  InsertByteAfterCursor = "I",
 }
 
 type EditParams = {
@@ -18,14 +18,14 @@ type EditParams = {
   moveCursorRight: () => void;
   setAppState: SetStateFn<AppState>;
   enabled: boolean;
-}
+};
 export const useEdit = ({
   cursor,
   moveCursorRight,
   bufferCommands,
   buffer,
   setAppState,
-  enabled
+  enabled,
 }: EditParams) => {
   const [isMSN, setIsMSN] = useState(true);
 
@@ -33,47 +33,53 @@ export const useEdit = ({
     setIsMSN(true);
   }, [cursor, buffer]);
 
-  useInput((input, key) => {
-    if (isHexChar(input)) {
-      const value = parseInt(input, 16);
-      if (isMSN) {
-        const newByte = (value << 4) | (buffer[cursor] & 0x0f);
-        bufferCommands.updateAtCursor(newByte);
-      } else {
-        const newByte = (buffer[cursor] & 0xf0) | value;
-        bufferCommands.updateAtCursor(newByte);
-        moveCursorRight();
-      }
-      setIsMSN(!isMSN);
-      return;
-    }
+  useInput(
+    (input, key) => {
+      if (isHexChar(input)) {
+        const value = parseInt(input, 16);
 
-    if (input === '?') {
-      return setAppState(AppState.Help);
-    }
+        if (!value) return;
 
-    if (input === 'j') {
-      return setAppState(AppState.Jump);
-    }
-
-    if (key.delete || key.backspace) {
-      bufferCommands.delete();
-      return;
-    }
-
-    switch (input) {
-      case CommandChar.InsertByteAtCursor: {
-        bufferCommands.insertAtCursor(new Uint8Array([0]));
+        if (isMSN) {
+          const newByte = (value << 4) | (buffer[cursor] & 0x0f);
+          bufferCommands.updateAtCursor(newByte);
+        } else {
+          const newByte = (buffer[cursor] & 0xf0) | value;
+          bufferCommands.updateAtCursor(newByte);
+          moveCursorRight();
+        }
+        setIsMSN(!isMSN);
         return;
       }
-      case CommandChar.InsertByteAfterCursor: {
-        bufferCommands.insertAfterCursor(new Uint8Array([0]));
+
+      if (input === "?") {
+        return setAppState(AppState.Help);
+      }
+
+      if (input === "j") {
+        return setAppState(AppState.Jump);
+      }
+
+      if (key.delete || key.backspace) {
+        bufferCommands.delete();
         return;
       }
-    }
 
-    if (key.ctrl && input === 's') {
-      return setAppState(AppState.Save);
-    }
-  }, {isActive: enabled});
-}
+      switch (input) {
+        case CommandChar.InsertByteAtCursor: {
+          bufferCommands.insertAtCursor(new Uint8Array([0]));
+          return;
+        }
+        case CommandChar.InsertByteAfterCursor: {
+          bufferCommands.insertAfterCursor(new Uint8Array([0]));
+          return;
+        }
+      }
+
+      if (key.ctrl && input === "s") {
+        return setAppState(AppState.Save);
+      }
+    },
+    { isActive: enabled }
+  );
+};
